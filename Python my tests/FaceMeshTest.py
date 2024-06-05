43aea03a64442fafd8a927c996e8cd87499f4c21
Adelcio Porto <portoinfo@sercomtel.com.br>
2024-06-05 17:13:29 -0300

    Implemented hands detector

--- Python my tests/FaceMeshTest.py
+++ Python my tests/FaceMeshTest.py
@@ -8,6 +8,7 @@ import cv2
 import mediapipe as mp
 import math
 import serial
+from cvzone.HandTrackingModule import HandDetector
 
 conectado = False
 porta = 'COM4'
@@ -155,14 +156,69 @@ class FaceMeshDetector:
 def main():
     cap = cv2.VideoCapture(0)
 
-    detector = FaceMeshDetector(staticMode=False, maxFaces=1, minDetectionCon=0.5, minTrackCon=0.5, initial_mouth_distance=0, initial_left_eye_distance=0)
+    detectorFace = FaceMeshDetector(staticMode=False, maxFaces=1, minDetectionCon=0.5, minTrackCon=0.5, initial_mouth_distance=0, initial_left_eye_distance=0)
+# Initialize the HandDetector class with the given parameters
+    detectorHands = HandDetector(staticMode=False, maxHands=2, modelComplexity=1, detectionCon=0.5, minTrackCon=0.5)
+
     while True:
         success, img = cap.read()
 
-        img, faces = detector.findFaceMesh(img, False)
+        img, faces = detectorFace.findFaceMesh(img, False)
         #if len(faces)!= 0:
         #   print(len(faces[0]))
 
+        # Find hands in the current frame
+        # The 'draw' parameter draws landmarks and hand outlines on the image if set to True
+        # The 'flipType' parameter flips the image, making it easier for some detections
+        hands, img = detectorHands.findHands(img, draw=True, flipType=True)
+        
+    # Check if any hands are detected
+    if hands:
+        # Information for the first hand detected
+        hand1 = hands[0]  # Get the first hand detected
+        lmList1 = hand1["lmList"]  # List of 21 landmarks for the first hand
+        bbox1 = hand1["bbox"]  # Bounding box around the first hand (x,y,w,h coordinates)
+        center1 = hand1['center']  # Center coordinates of the first hand
+        handType1 = hand1["type"]  # Type of the first hand ("Left" or "Right")
+
+        # Count the number of fingers up for the first hand
+        fingers1 = detectorHands.fingersUp(hand1)
+        print(f'H1 = {fingers1.count(1)}', end=" ")  # Print the count of fingers that are up
+
+        # Calculate distance between specific landmarks on the first hand and draw it on the image
+        length, info, img = detectorHands.findDistance(lmList1[8][0:2], lmList1[12][0:2], img, color=(255, 0, 255),
+                                                  scale=10)
+
+        finges1 = detectorHands.fingersUp(hands[0])
+        if finges1[0] == 0 and finges1[1] == 1 and finges1[2] == 0 and finges1[3] == 0 and finges1[4] == 0:
+           SerialArduino.write('1\n'.encode())      
+        if finges1[0] == 0 and finges1[1] == 0 and finges1[2] == 0 and finges1[3] == 0 and finges1[4] == 0:
+           SerialArduino.write('0\n'.encode())
+
+        # Check if a second hand is detected
+        if len(hands) == 2:
+            # Information for the second hand
+            hand2 = hands[1]
+            lmList2 = hand2["lmList"]
+            bbox2 = hand2["bbox"]
+            center2 = hand2['center']
+            handType2 = hand2["type"]
+
+            # Count the number of fingers up for the second hand
+            fingers2 = detectorHands.fingersUp(hand2)
+            print(f'H2 = {fingers2.count(1)}', end=" ")
+
+            # Calculate distance between the index fingers of both hands and draw it on the image
+            length, info, img = detectorHands.findDistance(lmList1[8][0:2], lmList2[8][0:2], img, color=(255, 0, 0),
+                                                      scale=10)
+
+            finges2 = detectorHands.fingersUp(hands[1])
+            if finges2[0] == 0 and finges2[1] == 1 and finges2[2] == 1 and finges2[3] == 0 and finges2[4] == 0:
+               SerialArduino.write('3\n'.encode())      
+            if finges2[0] == 0 and finges2[1] == 0 and finges2[2] == 0 and finges2[3] == 0 and finges2[4] == 0:
+               SerialArduino.write('2\n'.encode())
+
+        print(" ")  # New line for better readability of the printed output
 		# show camera image with defined points
         cv2.imshow("Image", img)
 
